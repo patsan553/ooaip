5adf30d4db6200af51a0f2567f62b40330a61b09
Ivan <146736443+patsan553@users.noreply.github.com>
2024-11-19 16:18:09 +0200

    Add files via upload

---
+++ Singleton/Computer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Computer
+    {
+        public Game CurrentGame { get; set; }
+        public void LaunchGame(string gameName)
+        {
+            CurrentGame = Game.GetInstance(gameName);
+        }
+    }
+}
---
+++ Singleton/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Game
+    {
+        private static Game instance;
+        public string Name { get; private set; }
+        private Game(string name)
+        {
+            this.Name = name;
+        }
+        public static Game GetInstance(string name)
+        {
+            if (instance == null)
+                instance = new Game(name);
+            return instance;
+        }
+    }
+}
