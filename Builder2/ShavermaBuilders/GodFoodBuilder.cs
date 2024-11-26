cb7789af6e47d07ec339f3f8bb48ae8f759f63a7
Ivan <146736443+patsan553@users.noreply.github.com>
2024-11-26 15:10:38 +0200

    Add files via upload

---
+++ Builder2/ShavermaBuilders/GodFoodBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Builder.Core;
+
+namespace Builder.ShavermaBuilders
+{
+    public abstract class GodFoodBuilder
+    {
+        public Shaverma GodFood { get; private set; }
+
+        public void CreateBread()
+        {
+            GodFood = new Shaverma();
+        }
+
+        public abstract void SetType();
+        public abstract void SetMeat();
+        public abstract void SetPepper();
+        public abstract void SetCabbage();
+        public abstract void SetMushroom();
+        public abstract void SetSauce();
+    }
+}
